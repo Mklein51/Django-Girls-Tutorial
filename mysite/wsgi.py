1e91b14b8c4425cbfc4cdd83001f119d5f030593
Mark Klein <Mklein51@ivytech.edu>
2025-04-28 21:23:54 -0400

    tried to correct path

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -6,11 +6,17 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
-
 import os
+import sys
 
+#  Add your Django project directory to the sys.path
+path = '/home/Mklein51/mklein51.pythonanywhere.com/djangogirls01'
+if path not in sys.path:
+    sys.path.append(path)
+
+#  Set Django settings module
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+
+#  Get the WSGI application
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-
 application = get_wsgi_application()
