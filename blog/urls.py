ab90108a6a7cad631d77604ec3dd51e50366eb30
Mark Klein <Mklein51@ivytech.edu>
2025-05-03 11:25:04 -0400

    Changed views again

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from django.contrib import admin
 from . import views 
 
-from django.contrib.auth import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
